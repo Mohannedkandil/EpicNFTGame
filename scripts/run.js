5b43fbebe789c20e301639d621e0f34a8078972e
Mohannedkandil <mohanadmkandil@gmail.com>
2022-03-06 18:22:47 +0200

    Update run.js to run the characters attributes locally

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,14 @@ const main = async () => {
     // Compile out contract and update the artifacts
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     // Hardhat will create Local Eth network
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Leo", "Ang", "Pikachu"], // Names
+        ["https://i.imgur.com/pKd5Sdk.png", // Images
+            "https://i.imgur.com/xVu4vFL.png",
+            "https://i.imgur.com/WMB6g9u.png"],
+        [100, 200, 300],                    // HP values
+        [100, 50, 25]                       // Attack damage values
+    );
     // Wait until the contract is deployed to our blockchain network, hardhat created fake miners
     // on my machine to try its best to imitate the actual blockchain
     await gameContract.deployed();
